352ee0030e9e0806a686b981657f73d2f59b86db
AleksandrVakarin <aleksandrvakarin7@gmail.com>
2024-01-21 12:34:37 +0500

    add primitivesMethods

---
+++ primitivesMethods.js
@@ -0,0 +1,19 @@
+'use strict'
+
+let obj = {
+	name: 'Jhon',
+	sayHi: function() {
+		alert('Hello')
+	},
+}
+
+obj.sayHi();
+
+let str = 'Hello';
+
+console.log(str.toUpperCase());
+
+let num = 1.23456;
+
+console.log(num.toFixed(2)); //1.23;
+
